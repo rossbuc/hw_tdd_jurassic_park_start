aa2ef9acdba6a59d0750ce038515877d378d208b
Ross Buchan <rossbuchan1@googlemail.com>
2023-08-08 22:15:47 +0100

    Add find best dino method

--- models/park.js
+++ models/park.js
@@ -12,6 +12,20 @@ class Park {
     removeDino(dino) {
         this.dinosaurCollection.pop()
     }
+
+    findBestDino() {
+        let bestDino = 0
+        for (let i = 0; i < this.dinosaurCollection.length - 1; i++) {
+            let thisDino = this.dinosaurCollection[i].guestsAttractedPerDay
+            let nextDino = this.dinosaurCollection[i + 1].guestsAttractedPerDay
+            console.log(thisDino)
+            console.log(nextDino)
+            if (thisDino > nextDino && thisDino > bestDino) {
+                bestDino = this.dinosaurCollection[i]
+            }
+        }
+        return bestDino
+    }
 }
 
 module.exports = Park
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -52,7 +52,10 @@ describe('Park', function() {
     assert.strictEqual(actual, 4)
   });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function() {
+    const actual = park.findBestDino()
+    assert.deepStrictEqual(actual, dino3)
+  });
 
   it('should be able to find all dinosaurs of a particular species');
 
